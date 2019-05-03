fd7192d31676a272312f3cd333f5550987a40086
Tariq <t.ali1993@hotmail.com>
2019-05-03 18:09:51 +0500

    Updates

---
+++ precache-manifest.aed65519b49a6acafd36897b9fb4f30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "429ee6142751381d2ef8",
+    "url": "/contactmanager/static/js/main.0569a446.chunk.js"
+  },
+  {
+    "revision": "9c5ad491ae6775c80596",
+    "url": "/contactmanager/static/js/2.812fc7c2.chunk.js"
+  },
+  {
+    "revision": "429ee6142751381d2ef8",
+    "url": "/contactmanager/static/css/main.c05a466b.chunk.css"
+  },
+  {
+    "revision": "9c5ad491ae6775c80596",
+    "url": "/contactmanager/static/css/2.1c401fa6.chunk.css"
+  },
+  {
+    "revision": "46d3504765a84d973e025ef548898d3e",
+    "url": "/contactmanager/index.html"
+  }
+];
